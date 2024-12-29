5059fa678cd08d82639171cd94058be1b9f6682d
acee3 <alexjakhangcheung@gmail.com>
2024-12-28 23:11:55 -0500

    started on vad feature

--- transcription-api/live_transcribe.py
+++ transcription-api/live_transcribe.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, request
+from speechbrain.inference.VAD import VAD
 import whisper
+import tempfile
 
+VAD = VAD.from_hparams(
+    source="speechbrain/vad-crdnn-libriparty"
+)
+print(VAD.get_speech_segments("preamble10.wav"))
 model = whisper.load_model("base")
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.post("/vad-segment")
+def vad_segment():
+    data = request.json
+    with tempfile.NamedTemporaryFile() as f:
+        f.write(data["audio"])
+        boundaries = VAD.get_speech_segments(f.name)
+    return {"text": boundaries}
+
+@app.post("/transcribe")
+def transcribe():
+    data = request.json
+    text = (model.transcribe(data["audio"]))[text]
+    return {"text": text}
